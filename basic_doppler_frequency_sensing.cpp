15948275d721d8a572d051a3f01e452cc5f23b9e
BENJAMINFRASER <Ben-fraser@hotmail.co.uk>
2017-06-22 21:11:33 +0100

    Updated comments on the Doppler basic measurement program.

--- basic_doppler_frequency_sensing.cpp
+++ basic_doppler_frequency_sensing.cpp
@@ -1,4 +1,15 @@
-
+/*************************************************************************
+ * Basic Doppler Frequency motion sensing program:                       *
+ *      A program to process an input waveform from a Doppler motion     *
+ *      sensing module, such as the HB100 or Parallax X-Band Radar       *
+ *      motion detector. It uses the FreqMeasure library, which needs    *
+ *      digital pin 8 or the Arduino UNO (or 49 on the MEGA)             *                                                                 *
+ *                                                                       *
+ *      Author: Sgt. B.D. Fraser                                         *
+ *                                                                       *
+ *        Last modified: 12/06/2017                                      *
+ *                                                                       *
+ *************************************************************************/
 
 // Freq Measure lib - uses digital pin 8 of Arduino Uno for measurement
 #include <FreqMeasure.h>
