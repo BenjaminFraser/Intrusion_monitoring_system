92417534de8c0c82f1f5bcec5b5387f42f5f7a49
BENJAMINFRASER <Ben-fraser@hotmail.co.uk>
2017-06-25 18:24:59 +0100

    Added header instructions and information to the master device communication program.

--- master_multiple_slave_communications_nrf24l01.cpp
+++ master_multiple_slave_communications_nrf24l01.cpp
@@ -1,7 +1,22 @@
+/*************************************************************************
+ * Master-unit - nRF24L01+ radio communications                          *
+ *      A program to operate as a master unit that transmits to and      *
+ *      receives data from three remote slave devices. Each slave        *
+ *      also has an nrf24l01+ transceiver, and replies with data         *
+ *      using the Acknowledgement payload facility of the Enhanced       *
+ *      ShockBurst packet structure.                                     *
+ *                                                                       *
+ *      Author: B.D. Fraser                                              *
+ *                                                                       *
+ *        Last modified: 25/06/2017                                      *
+ *                                                                       *
+ *************************************************************************/
+
 // include external libs for nrf24l01+ radio transceiver communications
-#include <RF24.h> 
-#include <SPI.h> 
+#include <RF24.h>
+#include <SPI.h>
 #include <nRF24l01.h>
+#include <printf.h>
 
 // set Chip-Enable (CE) and Chip-Select-Not (CSN) radio setup pins
 #define CE_PIN 9
@@ -15,10 +30,10 @@ RF24 radio(CE_PIN,CSN_PIN);
 
 // setup radio pipe addresses for each sensor node
 const byte smartNodeAddresses[3][5] = {
-                                        {'N','O','D','E','1'},
-                                        {'N','O','D','E','2'},
-                                        {'N','O','D','E','3'}
-                                       };
+    {'N','O','D','E','1'},
+    {'N','O','D','E','2'},
+    {'N','O','D','E','3'}
+};
 
 // integer to store count of successful transmissions
 int masterSendCount = 0;
@@ -35,31 +50,31 @@ unsigned long lastSentTime;
  */
 void setup()
 {
-  // setup serial communications for basic program display
-  Serial.begin(9600);
-  Serial.println("[*][*][*] Beginning nRF24L01+ master-multiple-slave program [*][*][*]");
-
-  // ----------------------------- RADIO SETUP CONFIGURATION AND SETTINGS -------------------------// 
-
-  // begin radio object
-  radio.begin();
-  
-  // set power level of the radio
-  radio.setPALevel(RF24_PA_LOW);
-
-  // set RF datarate - lowest rate for longest range capability
-  radio.setDataRate(RF24_250KBPS);
-
-  // set radio channel to use - ensure all slaves match this
-  radio.setChannel(0x76);
-
-  // set time between retries and max no. of retries
-  radio.setRetries(4, 10);
-
-  // enable ack payload - each slave replies with sensor data using this feature
-  radio.enableAckPayload();
-
-  // --------------------------------------------------------------------------------------------//
+    // setup serial communications for basic program display
+    Serial.begin(9600);
+    Serial.println("[*][*][*] Beginning nRF24L01+ master-multiple-slave program [*][*][*]");
+    
+    // ----------------------------- RADIO SETUP CONFIGURATION AND SETTINGS -------------------------//
+    
+    // begin radio object
+    radio.begin();
+    
+    // set power level of the radio
+    radio.setPALevel(RF24_PA_LOW);
+    
+    // set RF datarate - lowest rate for longest range capability
+    radio.setDataRate(RF24_250KBPS);
+    
+    // set radio channel to use - ensure all slaves match this
+    radio.setChannel(0x76);
+    
+    // set time between retries and max no. of retries
+    radio.setRetries(4, 10);
+    
+    // enable ack payload - each slave replies with sensor data using this feature
+    radio.enableAckPayload();
+    
+    // --------------------------------------------------------------------------------------------//
 }
 
 
@@ -74,7 +89,7 @@ void loop()
     
     // collect sensor data from all nodes
     receiveNodeData();
-
+    
     lastSentTime = millis();
 }
 
@@ -82,50 +97,50 @@ void loop()
 /* Function: receiveNodeData
  *    Make a radio call to each node in turn and retreive a message from each
  */
-void receiveNodeData() 
+void receiveNodeData()
 {
     Serial.print("[*] Master unit has successfully sent and received data ");
     Serial.print(masterSendCount);
     Serial.println(" times.");
-      
+    
     // make a call for data to each node in turn
     for (byte node = 0; node < 3; node++) {
-
+        
         // setup a write pipe to current sensor node - must match the remote node listening pipe
         radio.openWritingPipe(smartNodeAddresses[node]);
-
+        
         Serial.print("[*] Attempting to transmit data to node ");
         Serial.println(node + 1);
         Serial.print("[*] The master unit count being sent is: ");
         Serial.println(masterSendCount);
-
+        
         // boolean to indicate if radio.write() tx was successful
         bool tx_sent;
         tx_sent = radio.write( &masterSendCount, sizeof(masterSendCount) );
-
+        
         // if tx success - receive and read smart-post ack reply
         if (tx_sent) {
             if (radio.isAckPayloadAvailable()) {
-
+                
                 // read ack payload and copy sensor status to remotePostData array
                 radio.read(&remoteNodeData[node], sizeof(remoteNodeData[node]));
-
+                
                 Serial.print("[+] Successfully received data from node: ");
                 Serial.println(node);
                 Serial.print("  ---- The node count received was: ");
-                Serial.println(remoteNodeData[node][1]); 
-
+                Serial.println(remoteNodeData[node][1]);
+                
                 // iterate command unit count
                 if (masterSendCount < 500) {
                     masterSendCount++;
                 } else {
-                  masterSendCount = 1;
+                    masterSendCount = 1;
                 }
             }
         }
         else {
-          Serial.print("[-] The transmission to the selected node failed.");
+            Serial.print("[-] The transmission to the selected node failed.");
         }
     }
     Serial.println("--------------------------------------------------------");
- }
+}
