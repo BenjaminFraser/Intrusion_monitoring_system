664278dd647cb524134404460eee88cce877f19d
BENJAMINFRASER <Ben-fraser@hotmail.co.uk>
2017-06-26 12:58:47 +0100

    Updated comments and formatting of basic nrf24l01+ ack-payload comms master and slave files.

--- basic_communications_nRF24L01_master_one_slave.cpp
+++ basic_communications_nRF24L01_master_one_slave.cpp
@@ -4,17 +4,18 @@
  *      receives data from just one slave device. The slave              *
  *      also has an nrf24l01+ transceiver, and replies with data         *
  *      using the Acknowledgement payload facility of the Enhanced       *
- *      ShockBurst packet structure.                                     *
+ *      ShockBurst packet structure. The TMRh20 RF24 library is used     *                                  
+ *      for radio operation throughout                                   *
  *                                                                       *
  *      Author: B.D. Fraser                                              *
  *                                                                       *
- *        Last modified: 25/06/2017                                      *
+ *        Last modified: 26/06/2017                                      *
  *                                                                       *
  *************************************************************************/
 
 // include external libs for nrf24l01+ radio transceiver communications
-#include <RF24.h>
-#include <SPI.h>
+#include <RF24.h> 
+#include <SPI.h> 
 #include <nRF24l01.h>
 #include <printf.h>
 
@@ -46,91 +47,91 @@ unsigned long lastSentTime;
  */
 void setup()
 {
-    // setup serial communications for basic program display
-    Serial.begin(9600);
-    Serial.println("[*][*][*] Beginning nRF24L01+ master-single slave program [*][*][*]");
-    
-    // ----------------------------- RADIO SETUP CONFIGURATION AND SETTINGS -------------------------//
-    
-    // begin radio object
-    radio.begin();
-    
-    // set power level of the radio
-    radio.setPALevel(RF24_PA_LOW);
-    
-    // set RF datarate - lowest rate for longest range capability
-    radio.setDataRate(RF24_250KBPS);
-    
-    // set radio channel to use - ensure all slaves match this
-    radio.setChannel(0x66);
-    
-    // set time between retries and max no. of retries
-    radio.setRetries(4, 10);
-    
-    // enable ack payload - each slave replies with sensor data using this feature
-    radio.enableAckPayload();
-    
-    // setup a write pipe to remote node - must match the node listening pipe
-    radio.openWritingPipe(nodeAddress);
-    
-    // --------------------------------------------------------------------------------------------//
+  // setup serial communications for basic program display
+  Serial.begin(9600);
+  Serial.println("[*][*][*] Beginning nRF24L01+ master-single slave program [*][*][*]");
+
+  // ----------------------------- RADIO SETUP CONFIGURATION AND SETTINGS -------------------------// 
+
+  // begin radio object
+  radio.begin();
+  
+  // set power level of the radio
+  radio.setPALevel(RF24_PA_LOW);
+
+  // set RF datarate - lowest rate for longest range capability
+  radio.setDataRate(RF24_250KBPS);
+
+  // set radio channel to use - ensure all slaves match this
+  radio.setChannel(0x66);
+
+  // set time between retries and max no. of retries
+  radio.setRetries(4, 10);
+
+  // enable ack payload - each slave replies with sensor data using this feature
+  radio.enableAckPayload();
+
+  // setup a write pipe to remote node - must match the node listening pipe
+  radio.openWritingPipe(nodeAddress);
+
+  // --------------------------------------------------------------------------------------------//
 }
 
 
 /* Function: loop
- *    main loop program for the command post - repeats continuously during system operation
+ *    main loop program for the master device - repeats continuously during system operation
  */
 void loop()
 {
-    // ensure we dont collect data from remote node faster than selected rate
+    // ensure we dont collect data from slave node faster than selected rate
     currentTime = millis();
     while (currentTime - lastSentTime <= SEND_RATE) {}
     
-    // collect sensor data from all nodes
+    // collect data from slave node
     receiveNodeData();
-    
+
     lastSentTime = millis();
 }
 
 
 /* Function: receiveNodeData
- *    Make a radio call to each node in turn and retreive a message from each
+ *    Make a radio call to node and retreive preloaded ack-payload
  */
-void receiveNodeData()
+void receiveNodeData() 
 {
     Serial.print("[*] Master unit has successfully sent and received data ");
     Serial.print(masterSendCount);
     Serial.println(" times.");
-    
+
     Serial.println("[*] Attempting to transmit data to remote node.");
     Serial.print("[*] The master unit count being sent is: ");
     Serial.println(masterSendCount);
-    
+
     // boolean to indicate if radio.write() tx was successful
     bool tx_sent;
     tx_sent = radio.write( &masterSendCount, sizeof(masterSendCount) );
-    
+
     // if tx success - receive and read smart-post ack reply
     if (tx_sent) {
         if (radio.isAckPayloadAvailable()) {
-            
+
             // read ack payload and copy sensor status to remotePostData array
             radio.read(&remoteNodeData, sizeof(remoteNodeData));
-            
+
             Serial.print("[+] Successfully received data from  remote node.");
             Serial.print("  ---- The received count was: ");
-            Serial.println(remoteNodeData[1]);
-            
+            Serial.println(remoteNodeData[1]); 
+
             // iterate command unit count
             if (masterSendCount < 500) {
                 masterSendCount++;
             } else {
-                masterSendCount = 1;
+              masterSendCount = 1;
             }
         }
     }
     else {
-        Serial.print("[-] The transmission to the selected node failed.");
+      Serial.print("[-] The transmission to the selected node failed.");
     }
     Serial.println("--------------------------------------------------------");
-}
+ }
--- basic_slave_node_nrf24l01_comms.cpp
+++ basic_slave_node_nrf24l01_comms.cpp
@@ -1,7 +1,7 @@
 /*************************************************************************
  * Remote node - nRF24L01+ radio communications                          *
  *      A program to operate a remote-node slave device that sends       *
- *      data to a command unit on a given request message. The radio     *            
+ *      data to a command unit on a given request message. The radio     *
  *      transceiver used is the nRF24L01+, and it operates using the     *
  *      TMRh20 RF24 library.                                             *
  *                                                                       *
@@ -17,16 +17,10 @@
 #include <nRF24L01.h>
 #include <printf.h>
 
-// define smart-post ID - post ID should be 1 less than the post number, i.e. post 1 = 0
-#define POST_ID 0
-
 // chip select and RF24 radio setup pins
 #define CE_PIN 9
 #define CSN_PIN 10
 RF24 radio(CE_PIN,CSN_PIN);
-
-// int array to store incoming command post data: commandData = {systemCount, systemReset}
-int commandData[2] = {0};
 
 // setup radio pipe addresses for each sensor node
 const byte nodeAddress[5] = {'N','O','D','E','1'};
@@ -42,36 +36,36 @@ int dataFromMaster = 0;
  *    Initialises the system wide configuration and settings prior to start
  */
 void setup() {
-
-  Serial.begin(9600);
-
-  // ----------------------------- RADIO SETUP CONFIGURATION AND SETTINGS -------------------------// 
-  
-  radio.begin();
-  
-  // set power level of the radio
-  radio.setPALevel(RF24_PA_LOW);
-
-  // set RF datarate
-  radio.setDataRate(RF24_250KBPS);
-
-  // set radio channel to use - ensure it matches the target host
-  radio.setChannel(0x66);
-
-  radio.openReadingPipe(1, nodeAddress);     
-
-  // enable ack payload - smart posts reply with data using this feature
-  radio.enableAckPayload();
-  radio.writeAckPayload(1, &remoteNodeData, sizeof(remoteNodeData));
-
-  // print radio config details to console
-  printf_begin();
-  radio.printDetails();
-
-  // start listening on radio
-  radio.startListening();
-  
-  // --------------------------------------------------------------------------------------------//
+    
+    Serial.begin(9600);
+    
+    // ----------------------------- RADIO SETUP CONFIGURATION AND SETTINGS -------------------------//
+    
+    radio.begin();
+    
+    // set power level of the radio
+    radio.setPALevel(RF24_PA_LOW);
+    
+    // set RF datarate
+    radio.setDataRate(RF24_250KBPS);
+    
+    // set radio channel to use - ensure it matches the target host
+    radio.setChannel(0x66);
+    
+    radio.openReadingPipe(1, nodeAddress);
+    
+    // enable ack payload - smart posts reply with data using this feature
+    radio.enableAckPayload();
+    radio.writeAckPayload(1, &remoteNodeData, sizeof(remoteNodeData));
+    
+    // print radio config details to console
+    printf_begin();
+    radio.printDetails();
+    
+    // start listening on radio
+    radio.startListening();
+    
+    // --------------------------------------------------------------------------------------------//
 }
 
 
@@ -79,9 +73,9 @@ void setup() {
  *    main loop program for the slave node - repeats continuously during system operation
  */
 void loop() {
-  
-  // transmit current preloaded data to master device if message request received
-  radioCheckAndReply();
+    
+    // transmit current preloaded data to master device if message request received
+    radioCheckAndReply();
 }
 
 
@@ -89,17 +83,17 @@ void loop() {
  *    updates the count variable for the node and stores it in the nRF24L01+ radio
  *    preloaded ack payload ready for sending on next received message
  */
-void updateNodeData(void) 
+void updateNodeData(void)
 {
-  // increment node count - reset to 1 if exceeds 500
-  if (remoteNodeData[1] < 500) {
-    remoteNodeData[1]++;
-  } else {
-    remoteNodeData[1] = 1;
-  }
-
-  // set the ack payload ready for next request from the master device
-  radio.writeAckPayload(1, &remoteNodeData, sizeof(remoteNodeData));
+    // increment node count - reset to 1 if exceeds 500
+    if (remoteNodeData[1] < 500) {
+        remoteNodeData[1]++;
+    } else {
+        remoteNodeData[1] = 1;
+    }
+    
+    // set the ack payload ready for next request from the master device
+    radio.writeAckPayload(1, &remoteNodeData, sizeof(remoteNodeData));
 }
 
 
@@ -111,10 +105,10 @@ void radioCheckAndReply(void)
 {
     // check for radio message and send sensor data using auto-ack
     if ( radio.available() ) {
-          radio.read( &dataFromMaster, sizeof(dataFromMaster) );
-          Serial.println("Received request from master - sending preloaded data.");
-
-          // update the node count after sending ack payload - provides continually changing data
-          updateNodeData();
+        radio.read( &dataFromMaster, sizeof(dataFromMaster) );
+        Serial.println("Received request from master - sending preloaded data.");
+        
+        // update the node count after sending ack payload - provides continually changing data
+        updateNodeData();
     }
 }
